f688f55dae6e07538bf1ef7f303001ec31880df1
Favour919 <favourbassey999@gmail.com>
2022-09-04 08:46:51 +0100

    bar icon and images

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const navToggle = document.querySelector(".nav-toggle");
 const links = document.querySelector(".links");
+const barIcon = document.querySelector(".bar-icon");
 const socialIcons = document.querySelector('.social-icons')
 navToggle.addEventListener("click", function () {
-    links.classList.toggle("show-links");
+  links.classList.toggle("show-links");
+  
+});
+barIcon.addEventListener('click', function () {
+    if (barIcon.classList.contains('fa-bars')) {
+        barIcon.classList.replace('fa-bars', 'fa-times');
+    } else {
+        barIcon.classList.replace('fa-times', 'fa-bars');
+    }
 });
 // scroll bar
 const navbar = document.getElementById("nav");
